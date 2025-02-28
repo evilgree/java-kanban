2a150f711ae3072d884c401d8c4d3d775c8dcb73
evilgree <evilgree@yandex.ru>
2025-02-28 20:53:43 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Поехали!");
+        TaskManager taskManager = new TaskManager();
+
+        // Создание задач
+        Task task1 = taskManager.createTask(new Task("Задача 1", "Описание задачи 1"));
+        Task task2 = taskManager.createTask(new Task("Задача 2", "Описание задачи 2"));
+
+        Epic epic1 = taskManager.createEpic(new Epic("Эпик 1", "Описание эпика 1"));
+        Subtask subtask1 = taskManager.createSubtask(new Subtask("Подзадача 1", "Описание подзадачи 1", epic1.getId()));
+        Subtask subtask2 = taskManager.createSubtask(new Subtask("Подзадача 2", "Описание подзадачи 2", epic1.getId()));
+
+        Epic epic2 = taskManager.createEpic(new Epic("Эпик 2", "Описание эпика 2"));
+        Subtask subtask3 = taskManager.createSubtask(new Subtask("Подзадача 3", "Описание подзадачи 3", epic2.getId()));
     }
 }
---
+++ src/Subtask.java
@@ -0,0 +1,26 @@
+import java.util.Objects;
+
+class Subtask extends Task {
+    private int epicId;
+
+    public Subtask(String title, String description, int epicId) {
+        super(title, description);
+        this.epicId = epicId;
+    }
+
+    public int getEpicId() { return epicId; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Subtask)) return false;
+        if (!super.equals(o)) return false;
+        Subtask subtask = (Subtask) o;
+        return epicId == subtask.epicId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), epicId);
+    }
+}
